da9ab637ce9671cbbeccd41220fe99821240608e
ansuljain789 <ansuljain789@gmail.com>
2025-02-27 19:31:16 +0530

    added the msg routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const chats = require('./data/data');
 const userRoutes = require('./Routes/userRoutes')
 const chatRoutes= require('./Routes/chatRoutes');
 const {notFound,errorHandler} = require('./middleware/errorMiddleware')
+const messageRoutes = require('./Routes/messageRoutes')
 
 const connectDB = require('./config/db');
 require('dotenv').config();
@@ -34,6 +35,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
+app.use('/api/message',messageRoutes)
 
 app.use(notFound);
 app.use(errorHandler);
